b36a85ed23a54119df16e90c47ce31420bd1e4aa
philipp.winterle <philipp.winterle@check24.de>
2020-09-09 11:10:53 +0200

    added core tests

---
+++ tests/api.test.js
@@ -0,0 +1,62 @@
+const PageSpeeder = require("../Pagespeeder");
+let scores = null;
+
+beforeAll(async () => {
+  const ps = new PageSpeeder("https://github.com", "mobile");
+  scores = await ps.run();
+});
+
+describe("pagespeeder execution", () => {
+  test("pagespeeder result check", () => {
+    expect(Array.isArray(scores)).toBeTruthy();
+  });
+
+  test("pagespeeder result array has more then 0 entries", () => {
+    expect(scores.length).toBeGreaterThan(0);
+  });
+
+  test("scores contain device key and is mobile", () => {
+    const mobileScore = scores[0];
+    expect(mobileScore.device).toBe("mobile");
+  });
+
+  test("scores contain score key and is object", () => {
+    const mobileScore = scores[0];
+    expect(typeof mobileScore.score).toBe("object");
+  });
+
+  test("score obj contain actually scores", () => {
+    const mobileScore = scores[0];
+    const mainScore = mobileScore.score.mainScore;
+    expect(mainScore).toBeGreaterThan(0);
+  });
+
+  test("Auditscores contain core keys", () => {
+    const mobileScore = scores[0];
+    const audits = mobileScore.score.auditScores;
+    const expectedAuditKeys = [
+      "First Contentful Paint",
+      "Largest Contentful Paint",
+      "First Meaningful Paint",
+      "Speed Index",
+      "Estimated Input Latency",
+      "Total Blocking Time",
+      "Cumulative Layout Shift",
+      "First CPU Idle",
+      "Time to Interactive",
+    ];
+    expect(Object.keys(audits)).toEqual(
+      expect.arrayContaining(expectedAuditKeys)
+    );
+  });
+
+  test("Speed Index Audit contains 4 keys", () => {
+    const mobileScore = scores[0];
+    const audits = mobileScore.score.auditScores;
+    const testAudit = audits["Speed Index"];
+    const expectedAuditKeys = ["score", "value", "unit", "displayValue"];
+    expect(Object.keys(testAudit)).toEqual(
+      expect.arrayContaining(expectedAuditKeys)
+    );
+  });
+});
